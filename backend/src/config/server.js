507d69c1e13507ad1e0166c6ecf3055f73605c4b
Lucas Alves <lucasalves.s@outlook.com>
2019-01-21 21:51:26 -0200

    Finish add, list and delete tasks

--- backend/src/config/server.js
+++ backend/src/config/server.js
@@ -1,4 +1,5 @@
 const port = 3003
+
 const bodyParser = require('body-parser')
 const express = require('express')
 const server = express()
@@ -8,8 +9,8 @@ server.use(bodyParser.urlencoded({ extended: true }))
 server.use(bodyParser.json())
 server.use(allowCors)
 
-server.listen(port, function(){
-    console.log(`BACKEND is running on port ${port}`)
+server.listen(port, function() {
+    console.log(`BACKEND is running on port ${port}.`)
 })
 
 module.exports = server
